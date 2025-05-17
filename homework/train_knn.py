2aca5f0f668752b4904bfab5dcfcd179e7f09a54
samunozlo <samunozlo@unal.edu.co>
2025-05-17 12:41:59 -0500

    Taller 6

--- homework/train_knn.py
+++ homework/train_knn.py
@@ -10,10 +10,13 @@ import pandas as pd
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
+from homework.src.internals.calculate_metrics import calculate_metrics
+from homework.src.internals.save_model_if_better import save_model_if_better
 
 # descarga de datos
 url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
 df = pd.read_csv(url, sep=";")
+df.to_csv("data/winequality-red.csv")
 
 # preparacion de datos
 y = df["quality"]
@@ -36,10 +39,8 @@ print()
 print(estimator, ":", sep="")
 
 # Metricas de error durante entrenamiento
-y_pred = estimator.predict(x_train)
-mse = mean_squared_error(y_train, y_pred)
-mae = mean_absolute_error(y_train, y_pred)
-r2 = r2_score(y_train, y_pred)
+mse, mae, r2 = calculate_metrics(x_train, y_train, estimator)
+
 
 print()
 print("Metricas de entrenamiento:")
@@ -50,11 +51,10 @@ print(f"  R2: {r2}")
 # Metricas de error durante testing
 print()
 print("Metricas de testing:")
-y_pred = estimator.predict(x_test)
-mse = mean_squared_error(y_test, y_pred)
-mae = mean_absolute_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
+mse, mae, r2 = calculate_metrics(x_test, y_test, estimator)
 
 print(f"  MSE: {mse}")
 print(f"  MAE: {mae}")
 print(f"  R2: {r2}")
+
+save_model_if_better(estimator, x_test, y_test)
